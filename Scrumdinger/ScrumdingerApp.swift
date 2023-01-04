ce5448e962bd111e1a19e77c555240f28d679c25
J Rogel <j.rogel@rogueloop.com>
2023-01-04 12:34:03 +0000

    Navigation link

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -8,7 +8,9 @@ import SwiftUI
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: DailyScrum.sampleData)
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
         }
     }
 }
